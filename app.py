d6f168c6103aded8c588d804106bb884bfbf7765
defazr <thisiz43@gmail.com>
2025-06-04 20:50:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request
+import os
 import http.client
 import json
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 def fetch_movies():
     conn = http.client.HTTPSConnection("imdb-top-100-movies.p.rapidapi.com")
+
     headers = {
         "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY"),
         "X-RapidAPI-Host": os.getenv("RAPIDAPI_HOST")
@@ -15,20 +16,20 @@ def fetch_movies():
     conn.request("GET", "/top", headers=headers)
     res = conn.getresponse()
     data = res.read()
-    movies = json.loads(data)
+    movies = json.loads(data.decode("utf-8"))
+
+    # 문자열 리스트일 경우 title이 포함된 딕셔너리로 정리
+    if isinstance(movies, list) and isinstance(movies[0], str):
+        movies = [{"title": title} for title in movies]
+
     return movies
 
 @app.route('/')
 def index():
-    query = request.args.get('query', '')
+    query = request.args.get('query', '').lower()
     movies = fetch_movies()
+
     if query:
-        movies = [movie for movie in movies if query.lower() in movie['title'].lower()]
-    return render_template('index.html', movies=movies)
+        movies = [movie for movie in movies if query in movie.get('title', '').lower()]
 
-@app.route('/privacy-policy')
-def privacy():
-    return "<h1>Privacy Policy</h1><p>This is a sample privacy policy page.</p>"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("index.html", movies=movies)
